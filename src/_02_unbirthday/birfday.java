d5ee84bf88b0bf18b321440bbc4a2730549dce00
mcghee.mason <mcghee.mason@SEMS-259-35.SEMS259.LAB>
2019-11-13 14:52:58 -0800

    remarkable

---
+++ src/_02_unbirthday/birfday.java
@@ -0,0 +1,31 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class birfday {
+public static void main(String[] args) {
+	
+	String input = JOptionPane.showInputDialog("wat ur birfday ....... for nuting");
+
+	if( input.equals("11/13/19")) {	
+	JOptionPane.showMessageDialog(null,"happy birfdai do cog8tulatons");
+	}
+	
+	JOptionPane.showMessageDialog(null, "happy 0nbirfdaY");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
