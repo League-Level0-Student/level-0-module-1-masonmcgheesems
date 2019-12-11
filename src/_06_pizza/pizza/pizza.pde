ff4a10d357c306922ba0298663d23fe4542ea04f
Mcghee.mason <Mcghee.mason@SEMS-259-35.SEMS259.LAB>
2019-12-11 14:56:11 -0800

    processing zombie

--- src/_06_pizza/pizza/pizza.pde
+++ src/_06_pizza/pizza/pizza.pde
@@ -1,7 +1,11 @@
-
+PImage pepperoni;
 void setup() {
-    
+  pepperoni = loadImage("Pepperoni_Slice.jpg");
+    size(1000, 1000);
 }
 void draw() {
-    
+   fill(#622E04);
+ellipse(500,500,450,450);
+image(pepperoni, 300, 300,450,450);
+
 }
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -11,15 +11,21 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-JOptionPane.showInputDialog("What is as light as a feather, but even the world's strongest man couldn't hold it for more than a minute?); 
+String mia = JOptionPane.showInputDialog("What is as light as a feather, but even the world's strongest man couldn't hold it for more than a minute?"); 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(mia.equals("his breath")) {
+	JOptionPane.showMessageDialog(null,"coooooooooorrrrrrrreeeeeeeccccccttttttt!!!!!!!!");
+ score += 1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+JOptionPane.showMessageDialog(null, "wrong but this is an answer");
+JOptionPane.showMessageDialog(null,"the correct answer is his BREATH");
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		JOptionPane.showMessageDialog(null, "your score was"+ score + " great job");
+		JOptionPane.showMessageDialog(null, "your score was "+ score + " great job");
 	}
 
 
