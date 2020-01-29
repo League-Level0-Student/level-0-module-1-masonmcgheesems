09ba464dbcba4d792ea6cdbe975d58fa7d85f2b3
mcghee.mason <mcghee.mason@SEMS-259-35.SEMS259.LAB>
2020-01-29 14:56:16 -0800

    cp

--- src/_12_badgers/badger.java
+++ src/_12_badgers/badger.java
@@ -5,10 +5,16 @@ import javax.swing.JOptionPane;
 public class badger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	for (int i = 0; i < args.length; i++) {
-		
+	// TODO Auto-generated method stub
+		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 12; i++) {
+	System.out.println("badger"); 
+	}
+	for (int i = 0; i < 2; i++) {
+		System.out.println("mushroom");
+	}
+	}
 	}
 	}
 
-}
+
